8b928e3e6a06ec0a58ff947fb7ef82d8c2268be4
abhii6700 <abhishekyadav6700.ay@gmail.com>
2020-06-01 19:00:02 +0530

    initiate

---
+++ index.js
@@ -0,0 +1,18 @@
+var inputlabel = document.getElementById("inputlabel");
+var prevelement = document.getElementById("prevelement");
+function pushBtn(obj) {
+  var pushed = obj.innerHTML;
+
+  if (pushed == "=") {
+    prevelement.innerHTML = inputlabel.innerHTML + "=";
+    inputlabel.innerHTML = eval(inputlabel.innerHTML);
+  } else if (pushed == "AC") {
+    inputlabel.innerHTML = "0";
+  } else {
+    if (inputlabel.innerHTML == "0") {
+      inputlabel.innerHTML = pushed;
+    } else {
+      inputlabel.innerHTML += pushed;
+    }
+  }
+}
